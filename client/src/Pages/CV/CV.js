61f3d57e4a13878a88e46752e054177f701371e2
Arnab Majumdar <arnab@Arnabs-MacBook-Pro.local>
2019-01-29 12:59:16 -0600

    CV

--- client/src/Pages/CV/CV.js
+++ client/src/Pages/CV/CV.js
@@ -8,27 +8,59 @@ const CV = () =>
         <Jumbotron fluid> 
           <Container fluid className="font">
           
-          <h2> HIGHER EDUCATION EXPERIENCE</h2>
+          <h2 className="text-center"> HIGHER EDUCATION EXPERIENCE</h2>
           <hr/>
           <h4>Director, Adult, Veteran and Commuter Student Affairs </h4> 
-          <p className="floatLeft"> Depaul University, Chicago IL</p>
+          <p className="floatLeft">Depaul University, Chicago IL</p>
           <p className="floatRight">December 2016 – Present</p>
           <br/>
-          <div>
-          •	Lead operations of department, including advocacy, strategy, training, and budget.  
-•	Supervise Assistant Director, 6 coordinators who plan events and resources for assigned populations (for example students with children, graduate students, etc.), and in-directly supervise 6 Veteran Liaisons and 4 resource desk assistants.
-•	Second Certifying Official for Veteran benefits for DePaul University.
-•	Developed processes, design, and financing to add new Commuter Lounge and Veteran Student Lounge. 
-•	Oversaw creation of new Loop Student Resource Center as one-stop shop for Student Affairs inquiries in Loop (Downtown campus).
-•	Began new retention efforts such as Stop-Out Student Outreach for adult and veterans, Success Maps for Adult Students, redirected scholarship efforts, Veteran student coaching, and improved transition process.
-•	Contribute on Divisional Retention Task Force and lead effort to begin first-year and veteran coaching
-•	Launched new Commuter community-building team (EDGE) to create programming for commuters.
-•	Started new programming and event efforts to activate Loop (downtown) campus including Loop Block Party in Chicago city park which won Innovation Award within Student Affairs 2017
+          <ul className="clearFloat">
+          <li>  Lead operations of department, including advocacy, strategy, training, and budget.</li>
+          <li>  Supervise Assistant Director, 6 coordinators who plan events and resources for assigned populations (for example students with children, graduate students, etc.), and in-directly supervise 6 Veteran Liaisons and 4 resource desk assistants. </li>
+          <li>  Second Certifying Official for Veteran benefits for DePaul University.</li>
+          <li>  Developed processes, design, and financing to add new Commuter Lounge and Veteran Student Lounge.</li>
+          <li>  Oversaw creation of new Loop Student Resource Center as one-stop shop for Student Affairs inquiries in Loop (Downtown campus).</li>
+          <li>  Began new retention efforts such as Stop-Out Student Outreach for adult and veterans, Success Maps for Adult Students, redirected scholarship efforts, Veteran student coaching, and improved transition process.</li>
+          <li>  Contribute on Divisional Retention Task Force and lead effort to begin first-year and veteran coaching</li>
+          <li>  Launched new Commuter community-building team (EDGE) to create programming for commuters.</li>
+          <li>  Started new programming and event efforts to activate Loop (downtown) campus including Loop Block Party in Chicago city park which won Innovation Award within Student Affairs 2017</li>
+          </ul>
 
+          <br/><br/>
+          <h4>Assistant Director, Adult Student Affairs </h4> 
+          <p className="floatLeft">Depaul University, Chicago IL</p>
+          <p className="floatRight">January 2013 – December 2016</p>
+          <br/>
+          <ul className="clearFloat">
+          <li>Oversee day-to-day operation of the Adult Student Center, a facility and lounge that provides resources, services, and advocacy for non-traditional and graduate students.</li>
+          <li>Supervised 5 coordinators who plan events and resources for assigned Adult Student populations within Adult Students (for example students with children, graduate students, etc.)</li>
+          <li>Served as Acting Director of Adult, Veteran and Commuter Student Affairs January 2015-April 2015, January 2013-April 2013, and Interim Director September 2016-December 2016.</li>
+          <li>Obtained and executed a federally grant-funded study on students with children at DePaul. Chaired a task force and leading implementations of changes resulting from the study’s survey and focus groups.</li>
+          <li>Developed and oversaw first Conference for Students with Children, a one-day event with workshops and resources for that population which has become annual tradition. </li>
+          <li>Developed, led collaborations, and organized first Graduate Thesis and Dissertation conference, which has become annual event. </li>
+          <li>Served on divisional communication, assessment, and on-boarding committees. Served on division strategic planning committees in 2013 and 2016.</li>
+          <li>Served as Staff professional for Chicago Quarter Explorer Class in Fall 2014-2018.</li>
+          </ul>
 
-
-          </div>
-        
+          <br/><br/>
+          <h4>Assistant Director, Student Development</h4> 
+          <p className="floatLeft">University of Illinois Chicago, Chicago IL</p>
+          <p className="floatRight">January 2010 – January 2013</p>
+          <br/>
+          <ul className="clearFloat">
+          <li>Oversaw day-to-day operation of the Commuter Student Resource Center, a facility that provides programs, services, and amenities for students who live off-campus.</li>
+          <li>Led the strategic planning effort that resulted in a soft opening of the Commuter Student Resource Center in a temporary space in March 2010 and opening in a 12,000 sq. ft. permanent location in August 2010. This included chairing the Commuter Student Advisory Committee that advised on programs and services.</li>
+          <li>Coordinated with a capital projects manager on the construction of the permanent space for the facility.</li>
+          <li>Partnered with Office of Sustainability to launch UIC iCarpool, an online ride-sharing service.</li>
+          <li>Oversaw 8 student staff, 1 graduate assistant, and for a period of time, 1 Civil Service employee who operate the facility of the Commuter Student Resource Center, which saw over 65,000 visits by over 3,500 different students, in the 2011-2012</li>
+          <li>Managed a budget of over $300,000 to operate the Commuter Student Resource Center and its sister office, Student Leadership Development and Volunteer Services.</li>
+          <li>Completed a comprehensive budget proposal for the Commuter Student Resource Center which included a self-study with the Council for the Advancement Standards in Higher Education and benchmarking.</li>
+          <li>Led the continuing development of programs within the Commuter Student Resource Center from a workshop series to a campus-wide Off Campus Housing Fair.</li>
+          <li>Developed first-year specific programming including a Commuter Connection Day, attended by over 300 students in 2012, and a Commuter Assistant peer-mentoring program.</li>
+          <li>Partnered with over 15 offices on-campus to better bring programs and opportunities to the commuting students and a number of outside agencies like Metropolitan Tenants Organization and Active Transportation Alliance.</li>
+          <li>Served on campus-wide committees including the Student Affairs Assessment Committee, Sustainability Strategic Planning Committee, Basketball Marketing Committee, University Judicial Board, and a professional development committee.</li>
+          <li>Served for two years as co-led of the Chancellor’s Student Service and Leadership Awards ceremony which recognizes service and leadership accomplishments of over 400 students each year.</li>
+          </ul>
           </Container>
         </Jumbotron>
       </Container>
