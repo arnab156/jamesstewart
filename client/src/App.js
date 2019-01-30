4b25e250bbf2e3b5fc2aaee640e3ae2a3aeb3dff
Arnab Majumdar <arnab@Arnabs-MacBook-Pro.local>
2019-01-30 09:35:25 -0600

    routes to exact path

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Home from "./Pages/Home/Home";
 import NoMatch from "./Pages/NoMatch/NoMatch";
-// import Projects from "./Pages/Project/Project";
 import Navbar from "./Pages/Nav/Nav";
 import CV from "./Pages/CV/CV";
 import Edu from "./Pages/Education/Edu";
@@ -17,11 +16,10 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/home" component={Home} />
-        {/* <Route exact path="/projects" component={Projects} /> */}
-        <Route  path="/experience" component={CV} />
-        <Route  path="/education" component={Edu} />
-        <Route  path="/presentations" component={Presentation} />
-        <Route  path="/proaff" component={ProfesionalAff} />
+        <Route  exact path="/experience" component={CV} />
+        <Route  exact path="/education" component={Edu} />
+        <Route  exact path="/presentations" component={Presentation} />
+        <Route  exact path="/proaff" component={ProfesionalAff} />
         <Route component={NoMatch} />
       </Switch>
     </div>
